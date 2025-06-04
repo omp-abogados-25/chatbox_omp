ac8a4090e6fa530ce6d24082322611da6b116421
Deiber Olivares <olivaresdei6@gmail.com>
2025-06-04 12:47:17 -0500

    chore: Initial NestJS project setup and core application files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WhatsappWebhookPresentationModule } from './whatsapp-webhook/presentation';
+import { PositionModule } from './modules/positions';
+import { PrismaModule } from './integrations/prisma';
+import { RoleFunctionModule } from './modules/role-functions';
+
+const presentationModules = [PrismaModule, WhatsappWebhookPresentationModule, PositionModule, RoleFunctionModule];
 
 @Module({
-  imports: [],
+  imports: presentationModules,
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
+import { configDotenv } from 'dotenv';
+configDotenv();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  // documentacion de swagger
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('API para la aplicación')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  
   await app.listen(3000);
 }
 bootstrap();
